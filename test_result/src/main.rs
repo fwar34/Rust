69bf96170243a2c8bc059d4db165027ab9cc96b8
fwar34 <fwar34@126.com>
2018-12-08 22:31:07 +0800

    add test result

--- test_result/src/main.rs
+++ test_result/src/main.rs
@@ -1,4 +1,6 @@
 use std::fs::File;
+use std::io;
+use std::io::Read;
 use std::io::ErrorKind;
 
 fn main() {
@@ -14,4 +16,48 @@ fn main() {
             other_error => panic!("There was a problem opening the file: {:?}", other_error),
         },
     };
+}
+
+fn open_file() {
+    let f = File::open("hello.txt").map_err(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Tried to create file but there was a problem: {:?}", error);
+            })
+        } else {
+            panic!("There was a problem opening the file: {:?}", error);
+        }
+    });
+}
+
+fn open_file2(path: &String) {
+    let f = File::open(path).unwrap();
+}
+
+#[warn(unused_variables)]
+fn open_file3(path: &String) {
+    let f = File::open(path).expect("Failed to open hello.txt");
+}
+
+fn open_file4(path: &String) -> Result<String, io::Error> {
+    let f = File::open(path);
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(error) => return Err(error),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn open_file5(path: &String) -> Result<String, io::Error> {
+    let mut f = File::open(path)?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
 }
