17fdeebf3c03d8aa582445dd9a9461b6fb91ac0b
fwar34 <fwar34@126.com>
2018-12-10 22:07:08 +0800

    add generic

---
+++ generic/src/main.rs
@@ -0,0 +1,96 @@
+fn main() {
+    let number = vec![32, 1, -3, 100];
+    let result = largest_i32(&number);
+    println!("Largest num : {}", result);
+
+    let point1 = Point {x: 10, y: 11};
+    let point2 = Point {x: 1.0, y: 5.5};
+
+    let point3 = Point2 {x: 1, y: 5.6};
+    let point4 = Point2 {x: 5.3, y: 10};
+
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.x());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summary());
+}
+
+fn largest_i32(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+struct Point2<T, U> {
+    x: T,
+    y: U,
+}
+
+pub trait Summarizable {
+    fn summary(&self) -> String;
+}
+
+pub struct NewArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summarizable for NewArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} {}", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{} : {}", self.username, self.content)
+    }
+}
