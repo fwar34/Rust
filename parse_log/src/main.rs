414a3cf5400686332655bc951a56926e33b1148e
fwar34 <fwar34@126.com>
2022-04-20 23:16:39 +0800

    update

--- parse_log/src/main.rs
+++ parse_log/src/main.rs
@@ -1,6 +1,14 @@
-use::env;
-use::fs;
+use std::io;
+
+fn guess() {
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+}
 
 fn main() {
     println!("Hello, world!");
+    guess();
 }
