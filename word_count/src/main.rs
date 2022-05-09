d6274f5443221972ac4ee40a7d8be864bc42b173
fwar34 <fwar34@126.com>
2022-05-09 17:59:43 +0800

    update

--- word_count/src/main.rs
+++ word_count/src/main.rs
@@ -8,37 +8,49 @@ use std::{
 fn main() {
     let mut args = args();
     args.next();
-    let mut word_map: HashMap<&str, u32> = HashMap::new();
-    let contents = fs::read_to_string(args.next().unwrap()).unwrap();
-    let vs: Vec<&str> = contents.split(' ').collect();
-    for s in vs {
+    let path = args.next().expect("path is empty!");
+    let mut word_map: HashMap<String, u32> = HashMap::new();
+    let contents = fs::read_to_string(&path).unwrap();
+    let vs = contents.split_whitespace();
+    vs.for_each(|s| {
         let s = s.trim();
-        if let Some(&count) = word_map.get(&s) {
-            word_map.insert(s, count + 1);
-        } else {
-            word_map.insert(s, 1);
-        }
-    }
+        *word_map.entry(s.to_string()).or_insert(0) += 1;
+    });
 
-    for (k, v) in word_map {
+    word_map.iter().for_each(|(k, v)| {
         println!("{}:{}", k, v);
-    }
+    });
 
     println!("Hello, world!");
+
+    let mut map2 = HashMap::new();
+    use_buf(&path, &mut map2);
 }
 
-fn file_word(path: &str, word_map: &mut HashMap<&str, u32>) {
-    let file = File::open(path).unwrap();
+#[allow(unused)]
+fn use_buf(path: &str, map: &mut HashMap<String, u32>) {
+    let file = File::open(path).expect("failed to open file");
     let reader = BufReader::new(file);
-    for line in reader.lines() {
-        if let Ok(line) = line {
-            let words: Vec<&str> = line.split(' ').collect();
-            for word in words {
-                let word = word.trim();
-                if let Some(&count) = word_map.get(word) {
-                    word_map.insert(word, count + 1);
-                }
-            }
-        }
+    reader.lines().for_each(|line| {
+        line.unwrap().split_whitespace().for_each(|s| {
+            *map.entry(s.trim().to_string()).or_insert(0) += 1;
+        });
+    });
+
+    map.iter().for_each(|(k, v)| {
+        println!("{}:{}", k, v);
+    })
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashMap;
+    #[test]
+    fn test_insert() {
+        let mut map: HashMap<&str, u32> = HashMap::new();
+        map.entry("key").or_insert(1);
+        assert_eq!(*map.get("key").unwrap(), 1);
+        *map.entry("key").or_insert(1) += 1;
+        assert_eq!(*map.get("key").unwrap(), 2);
     }
 }
